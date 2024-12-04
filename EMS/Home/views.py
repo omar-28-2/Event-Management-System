5d4796795a77228538d4d1ca90c448dea9fa9591
Omar <omarelhosany3@gmail.com>
2024-12-04 11:09:14 +0200

    update viwes and models

--- EMS/Home/views.py
+++ EMS/Home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import ContactMessage
 
 def index(request):
     return render(request, 'Home/index.html')
@@ -9,3 +11,16 @@ def about(request):
 def contact(request):
     return render(request, 'Home/contact.html')
 
+
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Save data to the database
+        ContactMessage.objects.create(name=name, email=email, message=message)
+
+        # Return a success message or redirect
+        return render(request, 'contact_success.html')  # Create this template for success
+    return render(request, 'contact.html')  # Replace with your contact page template
