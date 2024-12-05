d0db0f8ab7eb1adc29ad3210c47fb5eb49a52276
amir <mohamedamir4711@gmail.com>
2024-12-05 15:15:32 +0200

    add contact form

--- EMS/Home/urls.py
+++ EMS/Home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('submit-contact-form/', views.contactForm, name='contactForm'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
 ]
--- EMS/Home/views.py
+++ EMS/Home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+import re
+from .models import ContactSubmission
 
 def index(request):
     return render(request, 'Home/index.html')
@@ -8,6 +10,25 @@ def about(request):
 
 def contact(request):
     return render(request, 'Home/contact.html')
+
+def contactForm(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        email = request.POST.get("email", "").strip()
+        message = request.POST.get("message", "").strip()
+
+        if not name.isalpha() or len(name) < 3:
+            return render(request, "Home/contact.html", {"error": "Please enter a valid name."})
+        elif not re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", email):
+            return render(request, "Home/contact.html", {"error": "Please enter a valid email address."})
+        elif len(message) < 10:
+             return render(request, "Home/contact.html", {"error": "Please enter a message with at least 10 characters."})
+        else:
+            ContactSubmission.objects.create(name=name, email=email, message=message)
+            
+            return render(request, "Home/contact.html", {"success": True})
+        
+    return render(request, "Home/contact.html")
 
 def signup(request):
     return render(request, 'Home/signUp.html')
