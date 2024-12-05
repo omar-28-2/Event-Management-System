3aebea351f23ae65842b47f36923f6cbe68cbd38
Omar <omarelhosany3@gmail.com>
2024-12-05 11:44:59 +0200

    modify in pages

--- EMS/Home/admin.py
+++ EMS/Home/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import ContactMessage
-
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'submitted_at')
-    search_fields = ('name', 'email')
--- EMS/Home/models.py
+++ EMS/Home/models.py
@@ -1,10 +1,2 @@
-from django.contrib.auth.models import User
 from django.db import models
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.user.username
 
