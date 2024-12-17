4dadf007a03660acc930d8674bf55b5fb975a915
Omar <omarelhosany3@gmail.com>
2024-12-17 20:16:27 +0200

    every thing in users and Home is ok to push them

--- EMS/users/models.py
+++ EMS/users/models.py
@@ -1,21 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(models.Model):
-    USER_TYPE_CHOICES = (
-        ('vendor', 'Vendor'),
-        ('user', 'User'),
-    )
-
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=15)
-    password = models.CharField(max_length=100)
-    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    user_type = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.username
-
-
-
-
-
